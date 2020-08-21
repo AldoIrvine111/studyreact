cb6f8afdede86e782fce7f00b0ec33feda3ec532
AldoIrvine111 <aldoirvine@gmail.com>
2020-08-21 22:47:22 +1000

    Multiplae state variables

--- src/index.js
+++ src/index.js
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App() {
+  const [manager, setManager] = useState("Alex");
   const [status, setStatus] = useState("Open");
+  const [year, setYear] = useState(2050);
   return (
-    <div>
-      <h1>Status : {status} </h1>
-      <button onClick={() => setStatus("Open")}>Open</button>
-      <button onClick={() => setStatus("Closed")}>Closed</button>
-      <button onClick={() => setStatus("Back in 5")}>Break</button>
-    </div>
+
+    <>
+      <div>
+        <h1>{year}</h1>
+        <button onClick={() => setYear(year + 1)}>New Year!</button>
+      </div>
+
+      <div>
+        <h1>Manager on Duty: {manager}</h1>
+        <button onClick={() => setManager("Alex")}>Alex</button>
+        <button onClick={() => setManager("Aldo")}>Aldo</button>
+        <button onClick={() => setManager("Beatrice")}>Beatrice</button>
+      </div>
+
+      <div>
+        <h1>Status : {status} </h1>
+        <button onClick={() => setStatus("Open")}>Open</button>
+        <button onClick={() => setStatus("Closed")}>Closed</button>
+        <button onClick={() => setStatus("Back in 5")}>Break</button>
+      </div>
+    </>
   )
 }
 
