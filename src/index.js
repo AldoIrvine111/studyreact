52380e3a8383e981b074905132623b8c54ec3f88
AldoIrvine111 <aldoirvine@gmail.com>
2020-08-21 17:39:51 +1000

    Using properties to pass a data value

--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello() {
+function Hello( {library, message, number} ) {
+  console.log(library) ;
+  console.log(message) ;
+  console.log(number) ;
   return (
-  <div>
-    <h1>Welcome to React!</h1>
-    <p>Lets start learning</p>
-    <p>I'm using Component to write React UI</p>
-  </div>
+    <div>
+      <h1>Welcome to {library}!</h1>
+      <p>Lets start learning</p>
+      <p>{message}</p>
+      <p>{number} = this is the assigned number with props</p>
+      <p>I'm using Component to write React UI</p>
+    </div>
   )
 }
 
 ReactDOM.render(
-  <Hello /> ,
+  <Hello
+    library="Aldo's Page"
+    message="Have FUN!"
+    number={1} />,
   document.getElementById('root')
 );
 
