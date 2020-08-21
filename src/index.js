ccf4d0520078b833163bead3b8b49a6bb12f8a1e
AldoIrvine111 <aldoirvine@gmail.com>
2020-08-21 22:59:32 +1000

    useEffect hook function, to make web more interactive with alert, console log and any other cool stuff

--- src/index.js
+++ src/index.js
@@ -1,38 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App() {
-  const [manager, setManager] = useState("Alex");
-  const [status, setStatus] = useState("Open");
-  const [year, setYear] = useState(2050);
+function CheckBox() {
+  const [checked, setChecked] = useState(true);
+  
+  useEffect(() => {
+    alert(`checked: ${checked.toString()}`);
+  }) ;
+
+
+
   return (
-
     <>
-      <div>
-        <h1>{year}</h1>
-        <button onClick={() => setYear(year + 1)}>New Year!</button>
-      </div>
-
-      <div>
-        <h1>Manager on Duty: {manager}</h1>
-        <button onClick={() => setManager("Alex")}>Alex</button>
-        <button onClick={() => setManager("Aldo")}>Aldo</button>
-        <button onClick={() => setManager("Beatrice")}>Beatrice</button>
-      </div>
-
-      <div>
-        <h1>Status : {status} </h1>
-        <button onClick={() => setStatus("Open")}>Open</button>
-        <button onClick={() => setStatus("Closed")}>Closed</button>
-        <button onClick={() => setStatus("Back in 5")}>Break</button>
-      </div>
+      <input type="checkbox"
+        value={checked}
+        onChange={() => setChecked(checked => !checked)} />
+      {checked ? "checked" : "not checked"}
     </>
   )
 }
 
 ReactDOM.render(
-  <App />,
+  <CheckBox />,
   document.getElementById('root')
 );
 
